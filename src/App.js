e8f6481a3e68d3255eb5277786dc062d3420933a
Aastha <118296087+AASTHA9416@users.noreply.github.com>
2023-11-14 19:12:56 +0530

    react task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+// CSS file
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar count={count} resetCount={resetCount} />
+      <div className="content">
+        <h1 className="main-count">Count: {count}</h1>
+        <button className="increase-count" onClick={increaseCount}>
+          Increase Count
+        </button>
+      </div>
     </div>
   );
 }
