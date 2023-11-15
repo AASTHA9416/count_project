fd37ac4d8d397ef879bae0c5f71bc3e9478f97b1
Aastha <118296087+AASTHA9416@users.noreply.github.com>
2023-11-15 22:31:03 +0530

    lec 5

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 // App.js
 
-import React, { useState } from 'react';
-import Navbar from './Navbar';
 // CSS file
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increaseCount = () => {
-    setCount(count + 1);
-  };
-
-  const resetCount = () => {
-    setCount(0);
-  };
-
-  return (
-    <div>
-      <Navbar count={count} resetCount={resetCount} />
-      <div className="content">
-        <h1 className="main-count">Count: {count}</h1>
-        <button className="increase-count" onClick={increaseCount}>
-          Increase Count
-        </button>
-      </div>
+ return (
+ <>
+ <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">TextUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+       
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
+ </>
   );
 }
 
